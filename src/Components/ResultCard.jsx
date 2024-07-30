d3785f109e5897c051c594b7a45ac626ff11a76d
Lana Gomes <lana.gomes1919@gmail.com>
2024-07-30 20:16:14 -0300

    update.: Making component more dinamic

--- src/Components/ResultCard.jsx
+++ src/Components/ResultCard.jsx
@@ -1,4 +1,11 @@
-function ResultCard() {
-  return <div>ResultCard</div>;
+function ResultCard({ totalCartao, totalTipoGasto }) {
+  return (
+    <>
+      <div className="border-s-8 border-pink text-left px-3 pt-3 pb-1  mx-12 my-3 bg-white-whistestWhite">
+        <header className="text-4xl">{totalCartao}</header>
+        <label className="text-2xl text-pink">{totalTipoGasto}</label>
+      </div>
+    </>
+  );
 }
 export default ResultCard;
