c99c7b2b03f508a46161c44a1d81dcca2f554f49
Lana Gomes <lana.gomes1919@gmail.com>
2024-07-30 18:54:19 -0300

    add:adding clicable in the menu logo

--- src/Components/SummaryBalance.jsx
+++ src/Components/SummaryBalance.jsx
@@ -1,4 +1,40 @@
+import React, { useState, useRef } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import calendarLogo from "../Images/calendarLogo.png";
+import ResultCard from "./ResultCard";
+
 function SummaryBalance() {
-  return <div className="sm:w-full">SummaryBalance</div>;
+  const [startDate, setStartDate] = useState(new Date());
+  const datePickerRef = useRef(null);
+
+  const handleIconClick = () => {
+    datePickerRef.current.setOpen(true);
+  };
+
+  return (
+    <div className="flex flex-col justify-center  ">
+      <main className=" flex justify-around text-2xl p-2 m-7 mx-14 items-center border-2 border-blue-2 rounded ">
+        <div className="flex justify-center w-1/2 cursor-pointer">
+          <img
+            className="h-10 opacity-60"
+            src={calendarLogo}
+            alt="Icone de calendario"
+            onClick={handleIconClick}
+          />
+        </div>
+        <DatePicker
+          ref={datePickerRef}
+          className=" text-start font-bold  text-blue-2 w-11/12 "
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+          dateFormat="MM/yyyy"
+          showMonthYearPicker
+        />
+      </main>
+      <ResultCard />
+    </div>
+  );
 }
+
 export default SummaryBalance;
