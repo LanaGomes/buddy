69394ffed8876bcd49c825cbff785b512394974b
Lana Gomes <lana.gomes1919@gmail.com>
2024-08-04 12:17:45 -0300

    add.:Adding margin bottom

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,7 +1,7 @@
 function Button({ buttonText }) {
   return (
     <div>
-      <button className="text-xl py-3 px-7 mt-8 bg-radiantGreen text-blue-2 rounded-full">
+      <button className="text-xl py-3 px-7 mt-8 mb-2 bg-radiantGreen text-blue-2 rounded-full">
         {buttonText}
       </button>
     </div>
