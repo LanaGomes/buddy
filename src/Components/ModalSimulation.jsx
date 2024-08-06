c0b2e28127dde7abaa48527d40110e34e4e0866b
Lana Gomes <lana.gomes1919@gmail.com>
2024-08-06 10:49:30 -0300

    update.: Making modal message dinamic and reusable

--- src/Components/ModalSimulation.jsx
+++ src/Components/ModalSimulation.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ModalSimulation = ({
   show,
-  handleClose,
+
   handleYes,
   handleNo,
   resultMessage,
@@ -13,9 +13,11 @@ const ModalSimulation = ({
 
   return (
     <div className="fixed inset-0  text-blue-2 bg-blue-2 bg-opacity-80 flex items-center justify-center">
-      <div className="bg-white-hotWhite p-6 rounded-md shadow-md text-center">
+      <div className="bg-white-whistestWhite p-6 rounded-md shadow-md text-center">
         <h2 className="text-xl  font-bold mb-4">Resultado</h2>
-        <p className="mb-4">{resultMessage}</p>
+        <p className="mb-5 p-2 border-x-2 border-blue-1 bg-blue-3 text-lg">
+          {resultMessage}
+        </p>
         <p className="mb-4 font-semibold">Você deseja adicionar este gasto?</p>
         <div className="space-x-4">
           <button
@@ -25,7 +27,7 @@ const ModalSimulation = ({
             Sim
           </button>
           <button
-            className="px-4 py-2 bg-red text-white rounded-md hover:bg-red-700"
+            className="px-4 py-2 bg-red-strong text-white rounded-md hover:bg-red-strong-700"
             onClick={handleNo}
           >
             Não
